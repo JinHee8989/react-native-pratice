8db0db6498c47531fff870f1ca70421a600fe014
wlsgml8989 <wlsgml8989@gmail.com>
2022-03-08 00:20:07 +0900

    state practice

--- App.js
+++ App.js
@@ -8,12 +8,54 @@
 
 import React, {Component} from 'react';
 import {View,Text,StyleSheet} from 'react-native';
+import PropsChild from './propsChild';
 
 class App extends Component{
+  //state는 함수에서 호출할수없음, render() 밖에서 사용가능, 쉽게 변경되는 안되는 값임 
+  state = {
+    sampleText: 'Hello World',
+    sampleBoolean: true,
+    sampleNum: 1
+  }
+
+  inputText = () => (
+    this.state.sampleBoolean? 
+    <Text>sampleBoolean is true</Text> 
+    :
+    <Text>sampleBoolean is false</Text> 
+  )
+  changeState = () => {
+    if(!this.state.sampleBoolean){
+      this.setState({
+        sampleText: 'Text Changed!',
+        sampleBoolean: true
+      })
+
+    }else{
+      this.setState({
+        sampleText: 'Hello world!',
+        sampleBoolean: false
+        
+      })
+
+    }
+
+  }
+
+  onAdd=()=>{
+    this.setState(prevState => {
+      return {
+        sampleNum: prevState.sampleNum + 1
+      }
+    })
+      // sampleNum: sampleNum+1
+      // sampleNum: 100
+    
+  }
   render(){
     return(
       <View style={styles.background}>
-        <Text>Hello World</Text>
+         <PropsChild sText={this.state.sampleText} cState={this.changeState}/>
       </View>
     )
   }
